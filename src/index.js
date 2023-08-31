b54fee551d14a02a3d7f14e55a7d68c7085fe145
loyer7770168 <loyer7770168@gmail.com>
2023-08-31 17:06:41 +0300

    Lesson 3

--- src/index.js
+++ src/index.js
@@ -1,81 +1,14 @@
-console.log ("Hey, this is my first JS program")
-/* 'number' + 23 + 32 
-'number' + 23 = `number23` + 32 = number2332
+const userInput = prompt('Enter number');
+const parsedInput = parseInt(userInput);
+const result = parsedInput;
 
-41 + 1 + 'number'
-41 + 1 = 42 + `numder` = "42number"
-
-null + 1 = 1
-
-'five' + + 'two'
-+`two` = NaN 
-`five` + Nan = "fiveNan"
-
-2 && 7 = true && 7 = 7
-
-+'40' + +'2'= 40 + 2 = 42
-
-'10' - 5 === 6;
-`10` - 5 = 10 - 5 = 5
-5 ===6 = false
-
-true + false
-1 + 0 = 1
-
-
-`4px` - 3 
-NaN - 3 = NaN
-
-'4' - 3
-4 - 3 = 1
-
-'2' + 3 ** 2;
-3 ** 2 = 9
-`2` + 9 = "29"
-
-12 / '6' = 12 / 6 = 2
-
-23 + 42 + 'number' = 65 + `number` = "65number"
-
-'10' + (5 === 6) = 5 === 6 = false = `10` + false = "10false"
-
-'number' + 15 + 3 = `number` + 15 = "number15" + 3 = "number153"
-
-undefined + 1 = NaN
-
-'true' == true = string == Boolean = false
-
-false == 'false' = false == string = false
-
-null == '' = null == 0 = false
-
-3 ** (9 / 3) = 3 ** 3 = 27
-
-!!'false' == !!'true' = true == true = true
-
-0 || '0' && 1 = 0 || `0` = `0` && 1 = 1
-
-1 < 2 < 3
-1 < 2 = true
-2 < 3 = true
-
-'foo'+ + +'bar'
-`foo`+ + = `foo`
-+`bar` = NaN
-`foo` + `Nan` = "fooNan"
-
-1 < 2 > 3 
-1 < 2 = true
-2 > 3 = false
-
-(+null == false) < 1 = (0 == false) = true < 1 = false
-
-false && true || true = false && true = false || true = true
-
-false && (true || true) = (true || true) = false && true = false
-
-(+null == false) < 1 ** 5 = (0 == false) = true < 1 = false */
-
-
-
-
+if (parsedInput % 3 === 0 && parsedInput % 5 === 0) {
+    console.log('FizzBuzz');
+} else if (parsedInput % 3 === 0) {
+    console.log('Fizz');
+} else if (parsedInput % 5 === 0) {
+    console.log('Buzz');
+} else {
+    console.log(parsedInput);    
+}    
+    
