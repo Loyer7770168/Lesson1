e01a986c0c32bda8d0c2d2a1ec15607613767878
loyer7770168 <loyer7770168@gmail.com>
2023-09-04 15:22:55 +0300

    Lesson4

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-const userInput = prompt('Enter number');
-const parsedInput = parseInt(userInput);
-const result = parsedInput;
+const inputNumber = prompt("Введіть число: ");
+const parsedNumber = parseInt(inputNumber);
 
-if (parsedInput % 3 === 0 && parsedInput % 5 === 0) {
-    console.log('FizzBuzz');
-} else if (parsedInput % 3 === 0) {
-    console.log('Fizz');
-} else if (parsedInput % 5 === 0) {
-    console.log('Buzz');
+if (!isNaN(parsedNumber)) {
+    if (parsedNumber <= 1) {
+        console.log(`Число ${parsedNumber} не є простим числом.`);
+    } else {
+        let isPrime = true;
+        for (let i = 2; i <= Math.sqrt(parsedNumber); i++) {
+            if (parsedNumber % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            console.log(`Число ${parsedNumber} є простим числом.`);
+        } else {
+            console.log(`Число ${parsedNumber} не є простим числом.`);
+        }
+    }
 } else {
-    console.log(parsedInput);    
-}    
-    
+    console.log("Ви ввели некоректне число.");
+}
+
