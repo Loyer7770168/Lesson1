05f3bb160c314361b1164ea92c92356104400f9c
loyer7770168 <loyer7770168@gmail.com>
2023-10-11 22:20:37 +0300

    Lesson 13

--- src/index.js
+++ src/index.js
@@ -1,20 +1,61 @@
-function logArguments(fn) {
-  return function (...args) {
-    console.log('Аргументи функції:', args);
-    return fn(...args);
+function Student(firstName, lastName, birthYear, course) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.birthYear = birthYear;
+  this.course = course;
+  this.grades = [];
+  this.attendance = [];
+
+  this.addGrade = function (grade) {
+    this.grades.push(grade);
+  };
+
+
+  this.addAttendance = function (attended) {
+    this.attendance.push(attended);
+  };
+
+  this.getAverageGrade = function () {
+    if (this.grades.length === 0) {
+      return 0;
+    }
+    const sum = this.grades.reduce((total, grade) => total + grade, 0);
+    return sum / this.grades.length;
+  };
+
+  this.getAverageAttendance = function () {
+    if (this.attendance.length === 0) {
+      return 0;
+    }
+    const sum = this.attendance.reduce((total, attended) => total + attended, 0);
+    return (sum / this.attendance.length) * 100;
+  };
+
+  this.getNumberOfLectures = function () {
+    return this.attendance.length;
+  };
+
+
+  this.changeCourse = function (newCourse) {
+    this.course = newCourse;
+  };
+
+  this.getStudentInfo = function () {
+    return `Ім'я: ${this.firstName} ${this.lastName}, Рік народження: ${this.birthYear}, Курс: ${this.course}, Оцінки: ${this.grades.join(', ')}, Відвідування: ${this.attendance.join(', ')}`;
   };
 }
 
-function add(a, b) {
-  return a + b;
-}
+const student1 = new Student("Іван", "Петров", 1998, 2);
 
-const decoratedAdd = logArguments(add);
+student1.addGrade(85);
+student1.addGrade(92);
+student1.addAttendance(1);
+student1.addAttendance(1);
 
-console.log(decoratedAdd(2, 3));
-
-
-
+console.log(student1.getStudentInfo());
+console.log(`Середній бал: ${student1.getAverageGrade()}`);
+console.log(`Середнє відвідування: ${student1.getAverageAttendance()}%`);
+console.log(`Кількість пройдених занять: ${student1.getNumberOfLectures()}`);
 
 
 
