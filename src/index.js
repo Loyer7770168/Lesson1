b431fb90da1545919088a119f2364073be6e58d7
loyer7770168 <loyer7770168@gmail.com>
2023-10-11 22:30:34 +0300

    Lesson 14-1

--- src/index.js
+++ src/index.js
@@ -1,61 +1,84 @@
-function Student(firstName, lastName, birthYear, course) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.birthYear = birthYear;
-  this.course = course;
-  this.grades = [];
-  this.attendance = [];
+class TodoList {
+  constructor() {
+    this.notes = [];
+  }
 
-  this.addGrade = function (grade) {
-    this.grades.push(grade);
-  };
-
-
-  this.addAttendance = function (attended) {
-    this.attendance.push(attended);
-  };
-
-  this.getAverageGrade = function () {
-    if (this.grades.length === 0) {
-      return 0;
+  addNote(noteText) {
+    if (noteText.trim() !== '') {
+      const note = {
+        text: noteText,
+        completed: false,
+      };
+      this.notes.push(note);
+      return 'Нотатка додана.';
+    } else {
+      return 'Нотатка не може бути порожньою.';
     }
-    const sum = this.grades.reduce((total, grade) => total + grade, 0);
-    return sum / this.grades.length;
-  };
+  }
 
-  this.getAverageAttendance = function () {
-    if (this.attendance.length === 0) {
-      return 0;
+  deleteNote(index) {
+    if (index >= 0 && index < this.notes.length) {
+      this.notes.splice(index, 1);
+      return 'Нотатка видалена.';
+    } else {
+      return 'Нотатка не існує за вказаним індексом.';
     }
-    const sum = this.attendance.reduce((total, attended) => total + attended, 0);
-    return (sum / this.attendance.length) * 100;
-  };
+  }
 
-  this.getNumberOfLectures = function () {
-    return this.attendance.length;
-  };
+  editNote(index, newText) {
+    if (index >= 0 && index < this.notes.length) {
+      if (newText.trim() !== '') {
+        this.notes[index].text = newText;
+        return 'Нотатку відредаговано.';
+      } else {
+        return 'Новий текст нотатки не може бути порожнім.';
+      }
+    } else {
+      return 'Нотатка не існує за вказаним індексом.';
+    }
+  }
 
+  getNoteInfo(index) {
+    if (index >= 0 && index < this.notes.length) {
+      const note = this.notes[index];
+      return `Нотатка #${index + 1}:\nТекст: ${note.text}\nВиконано: ${note.completed ? 'Так' : 'Ні'}`;
+    } else {
+      return 'Нотатка не існує за вказаним індексом.';
+    }
+  }
 
-  this.changeCourse = function (newCourse) {
-    this.course = newCourse;
-  };
+  markAsCompleted(index) {
+    if (index >= 0 && index < this.notes.length) {
+      this.notes[index].completed = true;
+      return 'Нотатку позначено як виконану.';
+    } else {
+      return 'Нотатка не існує за вказаним індексом.';
+    }
+  }
 
-  this.getStudentInfo = function () {
-    return `Ім'я: ${this.firstName} ${this.lastName}, Рік народження: ${this.birthYear}, Курс: ${this.course}, Оцінки: ${this.grades.join(', ')}, Відвідування: ${this.attendance.join(', ')}`;
-  };
+  getNoteCount() {
+    const totalNotes = this.notes.length;
+    const uncompletedNotes = this.notes.filter((note) => !note.completed).length;
+    return `Загальна кількість нотаток: ${totalNotes}, Невиконаних нотаток: ${uncompletedNotes}`;
+  }
+
+  getAllNotes() {
+    return this.notes.map((note, index) => `${index + 1}. ${note.text}`);
+  }
 }
 
-const student1 = new Student("Іван", "Петров", 1998, 2);
-
-student1.addGrade(85);
-student1.addGrade(92);
-student1.addAttendance(1);
-student1.addAttendance(1);
-
-console.log(student1.getStudentInfo());
-console.log(`Середній бал: ${student1.getAverageGrade()}`);
-console.log(`Середнє відвідування: ${student1.getAverageAttendance()}%`);
-console.log(`Кількість пройдених занять: ${student1.getNumberOfLectures()}`);
+const myTodoList = new TodoList();
+console.log(myTodoList.addNote('Приклад 1')); 
+console.log(myTodoList.addNote('Приклад 2')); 
+console.log(myTodoList.addNote(''));
+console.log(myTodoList.deleteNote(1)); 
+console.log(myTodoList.deleteNote(2));
+console.log(myTodoList.editNote(1, 'Новий текст')); 
+console.log(myTodoList.editNote(0, 'Новий текст')); 
+console.log(myTodoList.getNoteInfo(0)); 
+console.log(myTodoList.markAsCompleted(0)); 
+console.log(myTodoList.getNoteCount()); 
+console.log(myTodoList.getAllNotes()); 
 
 
 
