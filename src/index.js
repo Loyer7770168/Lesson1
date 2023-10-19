4947c53970437b86ea9277cc65de173a906f3f31
loyer7770168 <loyer7770168@gmail.com>
2023-10-19 10:33:13 +0300

    Lesson 16

--- src/index.js
+++ src/index.js
@@ -1,29 +1,20 @@
-function isPalindrome(number) {
-  const reverseNumber = parseInt(number.toString().split('').reverse().join(''), 10);
-  return number === reverseNumber;
-}
-
-function findPalindrome(seed) {
-  let currentNumber = seed;
-  let steps = 0;
-
-  while (steps < 1000) {
-    const reverseNumber = parseInt(currentNumber.toString().split('').reverse().join(''), 10);
-    const sum = currentNumber + reverseNumber;
-
-    if (isPalindrome(sum)) {
-      return { result: sum, steps };
+function randomDelayPrint(message) {
+  function printLetter(index) {
+    if (index < message.length) {
+      // Генеруємо випадкову затримку від 0 до 1000 мілісекунд (1 секунда)
+      const randomDelay = Math.floor(Math.random() * 1000);
+      
+      setTimeout(function () {
+        console.log(message[index]);
+        printLetter(index + 1);
+      }, randomDelay);
     }
-
-    currentNumber = sum;
-    steps++;
   }
 
-  return { result: null, steps: -1 };
+  printLetter(0);
 }
 
-const result = findPalindrome(196);
-console.log(result);
+randomDelayPrint("Hello");
 
 
 
