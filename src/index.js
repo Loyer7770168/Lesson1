d2b8974361677722b52dc5b7657bff2b1863bcb1
loyer7770168 <loyer7770168@gmail.com>
2023-10-30 06:55:48 +0200

    Lesson 18

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
-function sumArrayPromise(arr) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (Array.isArray(arr) && arr.length > 0) {
-        const sum = arr.reduce((acc, current) => acc + current, 0);
-        resolve(sum);
-      } else {
-        reject("Масив порожній або не є масивом чисел.");
-      }
-    }, 3000);
-  });
+function* fibonacci(n) {
+  let prev = 0;
+  let current = 1;
+  while (n > 0) {
+    yield prev;
+    const next = prev + current;
+    prev = current;
+    current = next;
+    n--;
+  }
 }
 
-sumArrayPromise([1, 2, 3, 4, 5])
-  .then(result => console.log(result))
-  .catch(error => console.error(error));
+const fibGen = fibonacci(10);
+
+console.log(fibGen.next().value); 
+console.log(fibGen.next().value);  
+console.log(fibGen.next().value); 
+console.log(fibGen.next().value); 
+console.log(fibGen.next().value); 
+
+
 
 
 
